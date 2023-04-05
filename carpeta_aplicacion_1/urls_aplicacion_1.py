f95ba5200af49ff0b7d0b852d080b7bb68270c09
zeroylalus <armopc8@gmail.com>
2023-04-05 10:20:46 -0600

    commit

--- carpeta_aplicacion_1/urls_aplicacion_1.py
+++ carpeta_aplicacion_1/urls_aplicacion_1.py
@@ -6,13 +6,31 @@
 
 # tambien podría ser 'from . import views' el punto significa ruta actual
 from django.urls import path
-from .views import view_funcion_hello
+
+from .views import view_funcion_hello_int
+from .views import view_funcion_hello_str
+
 from .views import view_funcion_acerca_de
 from .views import view_funcion_index
 
+from .views import view_funcion_proyectos
+from .views import view_funcion_tareas
+
 #función view_fun_hello puede extraer la variable var_params (especificar en típo de dato) en la ruta
 urlpatterns = [
+
+    # rutas de ejemplo:
+
+    path('string/<str:var_params_ruta>',view_funcion_hello_str),
+    path('entero/<int:var_params_ruta_int>',view_funcion_hello_int),
+
+    # rutas de datos:
+
     path('',view_funcion_index),
     path('ruta_acerca_de/', view_funcion_acerca_de),
-    path('hello/<str:var_params_ruta>',view_funcion_hello),
+
+    # rutas que van a recibir y enviar información:
+
+    path('proyectos/',view_funcion_proyectos),
+    path('tareas/',view_funcion_tareas),
 ]
--- carpeta_aplicacion_1/views.py
+++ carpeta_aplicacion_1/views.py
@@ -1,21 +1,49 @@
-# define que envia al cliente/navegador para ver en pantalla (archivos html)
-# ----- ARCHIVO ORIGINAL CREADO POR STARTAPP DJANGO ------------------
+""" ARCHIVO ORIGINAL CREADO POR STARTAPP DJANGO:
+. define que envia al cliente/navegador para ver en pantalla (archivos html)
+
+"""
 
 from django.shortcuts import render
-from django.http import HttpResponse
+# Jasonresponse, formato q entiende navegador, representar conjunto elementos (queryset)
+# ctrl + space : mostrar sugerencias
+from django.http import HttpResponse, JsonResponse
+from .models import clase_proyectos, clase_tareas
 
 # Create your views here.
 
-# funcion convencional con parámetro enviar repuesta HTTP
-# va a retornar HttpResponse, cuando se ejecute va a devolver al navegador
-# var var_params_ruta es una variable dentro de la ruta y debe tener el mismo nombre para su referencia
+"""
+. funcion convencional con parámetro que envía repuesta HTTP
+. función retorna HttpResponse, y cuando ejecute devuelve al navegador ('texto: hola isaac')
+. var var_params_ruta es una variable dentro de la ruta y debe tener el mismo nombre para su referencia
+. %s concatena un texto con una variante (crea un parametro que ira cambiando conforme cambio de una url a otra)
+"""
 
-def view_funcion_hello(view_funcion_hello_param_request, var_params_ruta):
-    return HttpResponse('texto: hola isaac')
+# --> funciones de ejemplo:
+
+def view_funcion_hello_str(view_funcion_hello_param_request, var_params_ruta):
+    print(type(var_params_ruta))
+    return HttpResponse("texto: " + var_params_ruta)
+
+def view_funcion_hello_int(view_funcion_hello_int_param_request, var_params_ruta_int):
+    return HttpResponse("<h1>texto: la ruta entero dice '%s'<h1>" % var_params_ruta_int)
+
+# --> funciones de datos:
 
 def view_funcion_acerca_de(view_funcion_acerca_param_request):
     return HttpResponse('texto: acerca de')
 
 def view_funcion_index(view_funcion_index_param_request):
     return HttpResponse('texto: index')
+
+# --> funciones que mostrarán y obtendran datos de proyectos y tareas
+
+def view_funcion_proyectos(view_funcion_proyectos_param_request):
+    # buscar, enviar, mostrar, etc. datos de database (atraves de parámetros)
+    # el query set para ser visualizado debe convertirse en lista y establecer el parametro "safe" a False
+    # .all() todos los "datos" y .values() solo los valores
+    queryset_proyectos = list(clase_proyectos.objects.values())
+    return JsonResponse(queryset_proyectos, safe=False)
+
+def view_funcion_tareas(view_funcion_tareas_param_request):
+    return HttpResponse('texto: tareas')
 
